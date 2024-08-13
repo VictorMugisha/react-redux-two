d8fa23a68408c09aaaa4f190b9d183ff7b00fd82
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-13 12:49:53 +0200

    Managed to cunsume all the slices in the redux store

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,10 +3,16 @@ import { useSelector } from 'react-redux';
 
 function Profile() {
     const user = useSelector(state => state.user.value)
+    const theme = useSelector(state => state.theme.value)
+
+    const dynamicStyles = {
+        color: theme ? theme : 'black'
+    }
+
     return (
         <div className="profile-container">
             <img src="profile-pic.jpg" alt="Profile" className="profile-pic" />
-            <h2 className="profile-name">John Doe - Username: {user.username}</h2>
+            <h2 className="profile-name" style={dynamicStyles}>John Doe - Username: {user.username}</h2>
             <p className="profile-bio">Software Developer at XYZ Corp - {user.email}</p>
         </div>
     );
